4071ec306d94a22d0f827706fa9f9f02279a9188
Decodedal <Palumbodallas@gmail.com>
2022-04-16 13:07:54 -0400

    next add total tracker

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("lam").addEventListener("click", function() {
+   console.log("ran lam")
+    window.prompt("Lamborghini Added to Cart");
+})
+
+document.getElementById("benz").addEventListener("click", function() {
+    console.log("ran benz")    
+    window.prompt("Mercedez - Benz Added to cart");
+    })
+        
+document.getElementById("x").addEventListener("click", function() {
+    console.log("ran x")        
+    window.prompt("model x added to cart");
+})
